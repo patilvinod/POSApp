439f3454d836adfe3c39e120537342193b4c9098
patilvinod <49274668+patilvinod@users.noreply.github.com>
2019-04-04 16:53:16 +0530

    Create Dependency Injection file

---
+++ DependencyInjector.cs
@@ -0,0 +1,34 @@
+using DataAccessLayer;
+using PosApplication.Interfaces;
+using POSApplication.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace POSApplication
+{
+    public static class DependencyInjector
+    {
+        private static readonly UnityContainer unityContainer = new UnityContainer();
+        public static IUnityContainer Register()
+        {
+            unityContainer.RegisterType<FrmRefuellerToRefueller>();
+            unityContainer.RegisterType<ILogin, LoginConcrete>();
+            unityContainer.RegisterType<IConfiguration, ConfigurationConcrete>();
+            unityContainer.RegisterType<IDailyAvailableFlights, DailyAvailableFlightsConcrete>();
+            unityContainer.RegisterType<IFuelTransaction, FuelTransactionConcrete>();
+            unityContainer.RegisterType<IScheduledFlightsSelf, ScheduledFlightSelfConcrete>();
+            unityContainer.RegisterType<IScheduledFlightOther, ScheduledFlightOtherConcrete>();
+            unityContainer.RegisterType<IFlowMeterCommunication, FlowMeterCommunicationConcrete>();
+            unityContainer.RegisterType<IImportData, ImportData>();
+            unityContainer.RegisterType<ICirculationTest, CirculationTestConcrete>();
+            unityContainer.RegisterType<IExportData, ExportData>();
+            unityContainer.RegisterType<IFuelEntryParameter, FuelEntryParameterConcrete>();
+            unityContainer.RegisterType<IStockTransfer, StockTransferConcrete>();
+            return unityContainer;
+        }
+    }
+}
